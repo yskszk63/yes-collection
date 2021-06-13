43adf35453caf4529be6ae0ea5b9333b27d99f2e
yskszk63 <yskszk63@gmail.com>
2021-06-13 19:32:51 +0900

    linebuf

--- c/yes.c
+++ c/yes.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-    setvbuf(stdout, 0, _IONBF, 0); // fair?
+    setvbuf(stdout, 0, _IOLBF, 0); // fair?
     for (;;) {
         puts("y");
     }
